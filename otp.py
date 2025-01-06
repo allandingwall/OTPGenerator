5fdda60953be6c6b85c6060155698d18e8959938
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-01-06 21:50:39 +1100

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,13 +1,13 @@
 import secrets
 
-msg = b"Hello world!"
-key = secrets.token_bytes(128)
+if __name__ == "__main__":
+    msg = b"Hello world!"
+    key = secrets.token_bytes(128)
+    enc_msg = bytes([msg[i] ^ key[i] for i in range(len(msg))])
 
-enc_msg = bytes([msg[i] ^ key[i] for i in range(len(msg))])
+    print(f"Message: {msg}")
+    print(f"Key: {key[:len(msg)]}")
+    print(f"Encrypted Message: {enc_msg}")
 
-print(f"Message: {msg}")
-print(f"Key: {key[:len(msg)]}")
-print(f"Encrypted Message: {enc_msg}")
-
-dec_msg = bytes([enc_msg[i] ^ key[i] for i in range(len(enc_msg))])
-print(f"Decrypted Message: {dec_msg}")
+    dec_msg = bytes([enc_msg[i] ^ key[i] for i in range(len(enc_msg))])
+    print(f"Decrypted Message: {dec_msg}")
