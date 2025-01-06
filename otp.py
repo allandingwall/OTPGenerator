fb073ad054718532ea89b37740a830b576bcabdf
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-01-06 21:49:24 +1100

    Create otp.py

---
+++ otp.py
@@ -0,0 +1,13 @@
+import secrets
+
+msg = b"Hello world!"
+key = secrets.token_bytes(128)
+
+enc_msg = bytes([msg[i] ^ key[i] for i in range(len(msg))])
+
+print(f"Message: {msg}")
+print(f"Key: {key[:len(msg)]}")
+print(f"Encrypted Message: {enc_msg}")
+
+dec_msg = bytes([enc_msg[i] ^ key[i] for i in range(len(enc_msg))])
+print(f"Decrypted Message: {dec_msg}")
