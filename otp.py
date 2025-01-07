073dfcef3fc2d28544e639cd875832344b56c2ed
Allan Dingwall <86416716+allandingwall@users.noreply.github.com>
2025-01-07 19:18:23 +1100

    Update otp.py

--- otp.py
+++ otp.py
@@ -6,16 +6,21 @@ def encrypt(plaintext, key):
     return cipher
 
 def decrypt(cipher, key):
-    pass
+    plaintext = bytes([cipher[i] ^ key[i] for i in range(len(cipher))])
+    return plaintext
 
-def generate_key():
-    pass
+def generate_key(key_length):
+    return secrets.token_bytes(key_length)
 
 def bytes_to_base64(byte_str):
     return base64.b64encode(byte_str)
 
 def base64_to_bytes(base64_str):
     return base64.b64decode(base64_str)
+
+def bytes_to_string(bytes_str):
+    string = bytes_str.decode("utf-8")
+    return string
 
 if __name__ == "__main__":
     msg = b"Hello world!"
@@ -26,5 +31,5 @@ if __name__ == "__main__":
     print(f"Key: {bytes_to_base64(key[:len(msg)])}")
     print(f"Encrypted Message: {bytes_to_base64(enc_msg)}")
 
-    dec_msg = bytes([enc_msg[i] ^ key[i] for i in range(len(enc_msg))])
+    dec_msg = decrypt(enc_msg, key)
     print(f"Decrypted Message: {dec_msg}")
